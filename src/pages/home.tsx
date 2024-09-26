44b975f1ff26e2cf99eadacf601a3e9553373507
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-26 13:25:33 -0500

    feat: separate filters and character list

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,14 @@
 import { Header } from "@/components/sections/header";
-import { Filters } from "@/components/sections/filter";
+import { Filters } from "@/components/sections/filters";
 import { Layout } from "@/components/ui/layout";
+import { CharacterList } from "@/components/sections/characterList";
 
 export default function Home() {
   return (
     <Layout>
       <Header />
       <Filters />
+      <CharacterList />
     </Layout>
   );
 }
