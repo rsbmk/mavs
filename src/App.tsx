3410e222c78dad5477978bb6176a7cc43bfbf2ae
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-26 13:23:58 -0500

    feat: implement router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-// import CharacterDetail from "./pages/character-detail";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import CharacterDetail from "./pages/character-detail";
 import Home from "./pages/home";
-// import Login from "./pages/login";
-// import Signup from "./pages/signup";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <Home />;
-  // return <Login />;
-  // return <Signup />;
-  // return <CharacterDetail />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/character/:id" element={<CharacterDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
