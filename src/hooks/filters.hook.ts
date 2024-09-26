3b77a82d2eeb56098eef6d9a1d4a0af295121d07
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-26 13:21:43 -0500

    feat: filters hook

---
+++ src/hooks/filters.hook.ts
@@ -0,0 +1,38 @@
+import { useSearchParams } from "react-router-dom";
+
+import { INPUTS_NAMES, CLEAR_SELECTION } from "@/constants";
+
+export function useFilters() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchDefaultVaule = searchParams.get(INPUTS_NAMES.SEARCH) || undefined;
+  const selectDefaultValue = searchParams.get(INPUTS_NAMES.SELECT_COMIC) || undefined;
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target as HTMLFormElement);
+    const search = formData.get(INPUTS_NAMES.SEARCH) as string;
+
+    if (search) {
+      searchParams.set(INPUTS_NAMES.SEARCH, search);
+    } else {
+      searchParams.delete(INPUTS_NAMES.SEARCH);
+    }
+
+    const comic = formData.get(INPUTS_NAMES.SELECT_COMIC) as string;
+
+    if (comic === CLEAR_SELECTION) {
+      searchParams.delete(INPUTS_NAMES.SELECT_COMIC);
+    } else {
+      searchParams.set(INPUTS_NAMES.SELECT_COMIC, comic);
+    }
+
+    setSearchParams(searchParams);
+  };
+
+  return {
+    searchDefaultVaule,
+    selectDefaultValue,
+    handleSearch
+  }
+}
