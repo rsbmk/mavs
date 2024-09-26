cf048491d75de3c7f673d2129878e07b809a277a
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-26 13:22:13 -0500

    chore: envs types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_MARVEL_PUBLIC_KEY: string
+  readonly VITE_MARVEL_PRIVATE_KEY: string
+  readonly VITE_MARVEL_BASE_URL: string
+  readonly VITE_MARVEL_HASH: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
