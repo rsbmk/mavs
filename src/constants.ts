292c37c97f319a122dafa97e7e82c6b101de3656
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-26 13:22:27 -0500

    feat: constants file

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const { VITE_MARVEL_PUBLIC_KEY, VITE_MARVEL_PRIVATE_KEY, VITE_MARVEL_BASE_URL, VITE_MARVEL_HASH } = import.meta.env
+
+export const INPUTS_NAMES = {
+  SEARCH: "nameStartsWith",
+  SELECT_COMIC: "comics"
+}
+
+export const CLEAR_SELECTION = "Clear filter"
