d0307ca2c63416d97a38ed237f3fbf6d7af8b06c
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-27 11:43:40 -0500

    feat: add comics list example

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,27 @@
-export const { VITE_MARVEL_PUBLIC_KEY, VITE_MARVEL_PRIVATE_KEY, VITE_MARVEL_BASE_URL, VITE_MARVEL_HASH } = import.meta.env
+export const { VITE_MARVEL_PUBLIC_KEY, VITE_MARVEL_PRIVATE_KEY, VITE_MARVEL_BASE_URL, VITE_MARVEL_HASH } = import.meta.env;
 
 export const INPUTS_NAMES = {
   SEARCH: "nameStartsWith",
-  SELECT_COMIC: "comics"
-}
+  SELECT_COMIC: "comics",
+};
 
-export const CLEAR_SELECTION = "Clear filter"
+export const CLEAR_SELECTION = "Clear filter";
+
+export const COMICS_LIST = [
+  {
+    id: 17548,
+    title: "Avengers (1998) #62",
+  },
+  {
+    id: 14328,
+    title: "X-Men (1991) #4",
+  },
+  {
+    id: 109961,
+    title: "Spider-Man (2022) #11",
+  },
+  {
+    id: CLEAR_SELECTION,
+    title: CLEAR_SELECTION,
+  },
+];
