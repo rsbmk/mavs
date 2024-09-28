86c4b24d7daafa5f8d6c631d06fa3762a27b070d
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-28 01:29:42 -0500

    refactor: load more character button style

--- src/components/sections/characterList.tsx
+++ src/components/sections/characterList.tsx
@@ -1,12 +1,14 @@
+import { Loader2 } from "lucide-react";
 import { useState } from "react";
 
-import { Character } from "./character";
 import { useQueryCharacters } from "@/module/character/infrastructure/character.query";
+import { Button } from "../ui/button";
+import { Character } from "./character";
 import { HeroListPlaceholder } from "./characterList.placeholder";
 
 export function CharacterList() {
   const [likedCharacters] = useState<Set<number>>(new Set());
-  const { data, isPending, isError } = useQueryCharacters();
+  const { data, isPending, isError, fetchNextPage, hasNextPage, isFetchingNextPage, isFetching } = useQueryCharacters();
 
   if (isPending) {
     return <HeroListPlaceholder />;
@@ -15,19 +17,24 @@ export function CharacterList() {
   if (isError) {
     return (
       <div className="flex justify-center items-center h-60">
-        <h3> We have an error</h3>
+        <h3>We have an error</h3>
       </div>
     );
   }
 
-  const { results: characters } = data;
+  const loadMoreButtonDisabled = !hasNextPage || isFetchingNextPage;
+  const isLoadingCharacters = isFetching || isFetchingNextPage;
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {characters.map((character) => {
-        const { id } = character;
-        return <Character character={character} isFav={likedCharacters.has(id)} key={id} />;
-      })}
-    </div>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {data.map((character) => (
+          <Character character={character} isFav={likedCharacters.has(character.id)} key={character.id + character.name} />
+        ))}
+      </div>
+      <Button className="block mx-auto my-4 disabled:cursor-not-allowed" disabled={loadMoreButtonDisabled} onClick={() => fetchNextPage()}>
+        {!hasNextPage ? "No more pages" : isLoadingCharacters ? <Loader2 className="animate-spin" /> : "Load more"}
+      </Button>
+    </>
   );
 }
