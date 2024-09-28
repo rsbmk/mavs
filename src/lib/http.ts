d27951d1be22fb356af0cb3e34985b2087c5af8c
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-28 03:38:27 -0500

    feat: create a mavs http

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,15 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { VITE_MARVEL_BASE_URL, VITE_MARVEL_HASH, VITE_MARVEL_PUBLIC_KEY } from '@/constants';
+import { VITE_MARVEL_BASE_URL, VITE_MARVEL_HASH, VITE_MARVEL_PUBLIC_KEY, VITE_MAVS_BASE_URL } from "@/constants";
 
 export const HTTP_MARVERL = axios.create({
   baseURL: VITE_MARVEL_BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
   params: {
     apikey: VITE_MARVEL_PUBLIC_KEY,
     hash: VITE_MARVEL_HASH,
-    ts: '1',
-  }
-})
+    ts: "1",
+  },
+});
+
+export const HTTP_MAVS = axios.create({
+  baseURL: VITE_MAVS_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${window.sessionStorage.getItem("jwt")}`,
+  },
+});
