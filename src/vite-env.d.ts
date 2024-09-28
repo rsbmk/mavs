166a13e01285454997cee5434fca6051c9563f92
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-28 03:38:00 -0500

    chore: add new envs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,13 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_MARVEL_PUBLIC_KEY: string
-  readonly VITE_MARVEL_PRIVATE_KEY: string
-  readonly VITE_MARVEL_BASE_URL: string
-  readonly VITE_MARVEL_HASH: string
+  readonly VITE_MARVEL_PUBLIC_KEY: string;
+  readonly VITE_MARVEL_PRIVATE_KEY: string;
+  readonly VITE_MARVEL_BASE_URL: string;
+  readonly VITE_MARVEL_HASH: string;
+  readonly VITE_MAVS_BASE_URL: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
