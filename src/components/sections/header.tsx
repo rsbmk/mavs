0ca8140ab1f0c1320f32144d92cb71358c9bd57e
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-29 15:56:06 -0500

    feat: toggle login/signup buttons with avatar

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -1,17 +1,27 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { useSession } from "@/module/auth/infrastructure/auth.query";
 
 export function Header() {
+  const { data: isSessionActive } = useSession();
   return (
     <header className="flex justify-between items-center mb-8">
       <h1 className="text-4xl font-bold">Marvel Characters</h1>
-      <div className="space-x-4">
-        <Button variant="outline">
-          <a href="/login">Login</a>
-        </Button>
-        <Button>
-          <a href="/signup">Sign Up</a>
-        </Button>
-      </div>
+      {isSessionActive ? (
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      ) : (
+        <div className="space-x-4">
+          <Button variant="outline">
+            <a href="/login">Login</a>
+          </Button>
+          <Button>
+            <a href="/signup">Sign Up</a>
+          </Button>
+        </div>
+      )}
     </header>
   );
 }
