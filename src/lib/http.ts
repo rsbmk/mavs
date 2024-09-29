4777f4665a09ec1300b6563b928758971dc76a42
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-29 15:51:35 -0500

    feat: add interceptor for 401 response delete the jwt session storage

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { VITE_MARVEL_BASE_URL, VITE_MARVEL_HASH, VITE_MARVEL_PUBLIC_KEY, VITE_MAVS_BASE_URL } from "@/constants";
+import { SESSION_STORAGE_KEYS, VITE_MARVEL_BASE_URL, VITE_MARVEL_HASH, VITE_MARVEL_PUBLIC_KEY, VITE_MAVS_BASE_URL } from "@/constants";
 
 export const HTTP_MARVERL = axios.create({
   baseURL: VITE_MARVEL_BASE_URL,
@@ -21,3 +21,13 @@ export const HTTP_MAVS = axios.create({
     Authorization: `Bearer ${window.sessionStorage.getItem("jwt")}`,
   },
 });
+
+HTTP_MAVS.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      window.sessionStorage.removeItem(SESSION_STORAGE_KEYS.JWT);
+    }
+    return Promise.reject(error);
+  }
+);
