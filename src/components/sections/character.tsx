2aa4b24d64986aa4a798df1f2ce530caf3e5e350
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-30 02:35:19 -0500

    fix: handle character like state

--- src/components/sections/character.tsx
+++ src/components/sections/character.tsx
@@ -1,9 +1,12 @@
 import type { Character } from "@/module/character/domain/character.type";
 
 import { Heart, Loader2 } from "lucide-react";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { useSession } from "@/module/auth/infrastructure/auth.query";
 import { useLikeMutation, useUnLikeMutation } from "@/module/like/infrastructure/like.mutation";
 
 type Props = {
@@ -15,10 +18,22 @@ export function Character({ character, likeId }: Props) {
   const { id, name, thumbnail, description } = character;
   const { mutate: onLike, isPending: isPendingLike } = useLikeMutation();
   const { mutate: onUnLike, isPending: isPendingUnLike } = useUnLikeMutation();
+  const { data: isSessionActive } = useSession();
+  const [isLike, setIsLike] = useState(Boolean(likeId));
 
   const toggleLike = (characterId: number) => () => {
-    const action = likeId ? onUnLike : onLike;
-    action({ characterId });
+    if (!isSessionActive) {
+      toast.warning("Please login to like characters");
+      return;
+    }
+
+    if (likeId) {
+      onUnLike({ id: likeId });
+    } else {
+      onLike({ characterId });
+    }
+
+    setIsLike((pre) => !pre);
   };
 
   const isPending = isPendingLike || isPendingUnLike;
@@ -34,9 +49,9 @@ export function Character({ character, likeId }: Props) {
         <p className="text-sm text-gray-600 line-clamp-4">{description || "No description"}</p>
       </CardContent>
       <CardFooter className="flex flex-col items-start gap-2">
-        <Button variant={likeId ? "default" : "outline"} size="sm" className="mt-2 gap-2" onClick={toggleLike(id)}>
-          {isPending ? <Loader2 className="animate-spin" size={16} /> : <Heart className={likeId ? "fill-current" : ""} size={16} />}
-          {likeId ? "Liked" : "Like"}
+        <Button variant={isLike ? "default" : "outline"} size="sm" className="mt-2 gap-2" onClick={toggleLike(id)}>
+          {isPending ? <Loader2 className="animate-spin" size={16} /> : <Heart className={isLike ? "fill-current" : ""} size={16} />}
+          {isLike ? "Liked" : "Like"}
         </Button>
       </CardFooter>
     </Card>
