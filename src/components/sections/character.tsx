244a46184507e18b5e316c534650a87d752f83ef
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-29 19:06:01 -0500

    feat: like and unlike funtionality

--- src/components/sections/character.tsx
+++ src/components/sections/character.tsx
@@ -1,44 +1,42 @@
 import type { Character } from "@/module/character/domain/character.type";
 
-import { Heart } from "lucide-react";
+import { Heart, Loader2 } from "lucide-react";
 
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { useLikeMutation, useUnLikeMutation } from "@/module/like/infrastructure/like.mutation";
 
 type Props = {
   character: Character;
-  isFav: boolean;
+  likeId?: string;
 };
 
-export function Character({ character, isFav }: Props) {
+export function Character({ character, likeId }: Props) {
   const { id, name, thumbnail, description } = character;
-  // const { items: comicsItems } = comics;
+  const { mutate: onLike, isPending: isPendingLike } = useLikeMutation();
+  const { mutate: onUnLike, isPending: isPendingUnLike } = useUnLikeMutation();
 
-  const toggleLike = (id: number) => {
-    // setLikedCharacters((prev) => {
-    //   const newSet = new Set(prev);
-    //   if (newSet.has(id)) {
-    //     newSet.delete(id);
-    //   } else {
-    //     newSet.add(id);
-    //   }
-    //   return newSet;
-    // });
+  const toggleLike = (characterId: number) => () => {
+    const action = likeId ? onUnLike : onLike;
+    action({ characterId });
   };
+
+  const isPending = isPendingLike || isPendingUnLike;
+  const src = `${thumbnail.path}.${thumbnail.extension}`;
 
   return (
     <Card key={id} className="overflow-hidden">
-      <img src={thumbnail.path + "." + thumbnail.extension} alt={name} className="w-full h-[300px] object-cover" />
+      <img src={src} alt={name} className="w-full h-[300px] object-cover" />
       <CardHeader>
         <CardTitle>{name}</CardTitle>
       </CardHeader>
       <CardContent>
-        <p className="text-sm text-gray-600">{description || "No description"}</p>
+        <p className="text-sm text-gray-600 line-clamp-4">{description || "No description"}</p>
       </CardContent>
       <CardFooter className="flex flex-col items-start gap-2">
-        <Button variant={isFav ? "default" : "outline"} size="sm" className="mt-2" onClick={() => toggleLike(id)}>
-          <Heart className={`mr-2 h-4 w-4 ${isFav ? "fill-current" : ""}`} />
-          {isFav ? "Liked" : "Like"}
+        <Button variant={likeId ? "default" : "outline"} size="sm" className="mt-2 gap-2" onClick={toggleLike(id)}>
+          {isPending ? <Loader2 className="animate-spin" size={16} /> : <Heart className={likeId ? "fill-current" : ""} size={16} />}
+          {likeId ? "Liked" : "Like"}
         </Button>
       </CardFooter>
     </Card>
--- src/components/sections/characterList.tsx
+++ src/components/sections/characterList.tsx
@@ -1,16 +1,16 @@
 import { Loader2 } from "lucide-react";
-import { useState } from "react";
 
 import { useQueryCharacters } from "@/module/character/infrastructure/character.query";
+import { userFindLikesByUserQuery } from "@/module/like/infrastructure/like.query";
 import { Button } from "../ui/button";
 import { Character } from "./character";
 import { HeroListPlaceholder } from "./characterList.placeholder";
 
 export function CharacterList() {
-  const [likedCharacters] = useState<Set<number>>(new Set());
+  const { data: likes, isPending: isPendingLikes, isError: isErrorLikes } = userFindLikesByUserQuery();
   const { data, isPending, isError, fetchNextPage, hasNextPage, isFetchingNextPage, isFetching } = useQueryCharacters();
 
-  if (isPending) {
+  if (isPending || isPendingLikes) {
     return <HeroListPlaceholder />;
   }
 
@@ -22,15 +22,24 @@ export function CharacterList() {
     );
   }
 
+  if (isErrorLikes) {
+    return (
+      <div className="flex justify-center items-center h-60">
+        <h3>We have an error getting the likes</h3>
+      </div>
+    );
+  }
+
   const loadMoreButtonDisabled = !hasNextPage || isFetchingNextPage;
   const isLoadingCharacters = isFetching || isFetchingNextPage;
 
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {data.map((character) => (
-          <Character character={character} isFav={likedCharacters.has(character.id)} key={character.id + character.name} />
-        ))}
+        {data.map((character) => {
+          const likeId = likes.find((like) => like.characterId === character.id)?.id;
+          return <Character character={character} likeId={likeId} key={character.id + character.name} />;
+        })}
       </div>
       <Button className="block mx-auto my-4 disabled:cursor-not-allowed" disabled={loadMoreButtonDisabled} onClick={() => fetchNextPage()}>
         {!hasNextPage ? "No more pages" : isLoadingCharacters ? <Loader2 className="animate-spin" /> : "Load more"}
