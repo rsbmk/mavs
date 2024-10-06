29866d3b5ea5a60bd9d91233a428c8b0edf05595
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-10-06 13:38:21 -0500

    feat: add react query dev tool
    feat: add url params contants

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { PATHS } from "./constants";
+import { PATHS, URL_PARAMS } from "./constants";
 import CharacterDetail from "./pages/character-detail";
 import Home from "./pages/home";
 import Login from "./pages/login";
@@ -26,10 +27,11 @@ function App() {
           <Route path={PATHS.HOME} element={<Home />} />
           <Route path={PATHS.LOGIN} element={<Login />} />
           <Route path={PATHS.SIGNUP} element={<Signup />} />
-          <Route path={`${PATHS.CHARACTER_DETAIL}:id`} element={<CharacterDetail />} />
+          <Route path={`${PATHS.CHARACTER_DETAIL}/:${URL_PARAMS.CHARACTER_ID}`} element={<CharacterDetail />} />
         </Routes>
         <ToastContainer />
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
