682ff05a40f74a16e603b4723622f02b26b6bbfd
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-10-06 17:49:48 -0500

    feat: get like by cache

--- src/module/like/infrastructure/like.query.ts
+++ src/module/like/infrastructure/like.query.ts
@@ -1,9 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { QUERY_KEYS } from "@/constants";
 
 import { useLikeByUser } from "../domain/like.funtions";
 import { findLikeByCharacterAndUser } from "../domain/like.respository";
+import { Like } from "../domain/like.type";
 
 /**
  * Find all likes of the current user.
@@ -25,10 +26,25 @@ export function useFindLikesByUser() {
  * @param {number} characterId - The id of the character.
  * @returns {Promise<Like>} The like or null if not found.
  */
-export function useFindOneLikesByUserAndCharacter(characterId?: string) {
+export function useFindOneLikesByUserAndCharacter(characterId: number) {
+  const queryClient = useQueryClient();
+
   return useQuery({
     queryKey: [QUERY_KEYS.LIKES_BY_USER, characterId],
     enabled: Boolean(characterId),
-    queryFn: () => findLikeByCharacterAndUser(Number(characterId)),
+    queryFn: async () => {
+      const queyKeyAllLikes = [QUERY_KEYS.LIKES_BY_USER];
+
+      const cache = queryClient.getQueryData<Like[]>(queyKeyAllLikes);
+      const like = cache?.find((like) => like.characterId === characterId);
+      if (like) return like;
+
+      /**
+       * if i have a cache, and the like is not there maybe not exist in the server
+       */
+      if (cache && cache.length > 0) return null;
+
+      return findLikeByCharacterAndUser(Number(characterId));
+    },
   });
 }
