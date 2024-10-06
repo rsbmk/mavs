f61d82c1098f9f9c4b4768e009ab6b387d210d28
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-10-06 13:42:10 -0500

    feat: obtimistic UI for like mutations

--- src/module/like/infrastructure/like.mutation.ts
+++ src/module/like/infrastructure/like.mutation.ts
@@ -1,28 +1,79 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+import { QUERY_KEYS } from "@/constants";
 import { handleError } from "@/module/common/utils";
-import { useLike, useUnLike } from "../domain/like.funtions";
 
+import { useLike, useUnLike } from "../domain/like.funtions";
+import { Like } from "../domain/like.type";
+
+/**
+ * React query mutation to like a character.
+ *
+ * @returns Mutation function to like a character.
+ */
 export function useLikeMutation() {
   const mutationFn = useLike();
+  const queryClient = useQueryClient();
+  const queryKeyLikes = [QUERY_KEYS.LIKES_BY_USER];
 
   return useMutation({
-    mutationKey: ["like"],
     mutationFn,
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries({ queryKey: queryKeyLikes });
+      const previousCache = queryClient.getQueryData<Like[]>(queryKeyLikes);
+      if (previousCache) {
+        const newLike: Like = {
+          id: crypto.randomUUID(),
+          characterId: variables.characterId,
+          userId: previousCache[0].userId,
+          createAt: new Date(),
+          deleteAt: null,
+          state: true,
+          updateAt: new Date(),
+        };
+        const newCache = [...previousCache, newLike];
+        queryClient.setQueryData(queryKeyLikes, newCache);
+      }
+      return { previousCache };
+    },
     onError(error, variables) {
       handleError(error, `An error has ocurred while like: ${variables.characterId}`);
+    },
+    onSettled: async () => {
+      await queryClient.invalidateQueries({ queryKey: queryKeyLikes });
     },
   });
 }
 
+/**
+ * React query mutation to unlike a character.
+ *
+ * @returns Mutation function to unlike a character.
+ */
 export function useUnLikeMutation() {
   const mutationFn = useUnLike();
+  const queryClient = useQueryClient();
+  const queryKeyLikes = [QUERY_KEYS.LIKES_BY_USER];
 
   return useMutation({
-    mutationKey: ["like"],
     mutationFn,
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries({ queryKey: queryKeyLikes });
+      const previousCache = queryClient.getQueryData<Like[]>(queryKeyLikes);
+      if (previousCache) {
+        const rmLike = previousCache.find((like) => like.id === variables.id);
+        queryClient.setQueryData([QUERY_KEYS.LIKES_BY_USER, String(rmLike?.characterId)], null);
+
+        const newCacheLikes = previousCache.filter((like) => like.id !== variables.id);
+        queryClient.setQueryData(queryKeyLikes, newCacheLikes);
+      }
+      return { previousCache };
+    },
     onError(error, variables) {
       handleError(error, `An error has ocurred while like: ${variables.id}`);
+    },
+    onSettled: async () => {
+      await queryClient.invalidateQueries({ queryKey: queryKeyLikes });
     },
   });
 }
