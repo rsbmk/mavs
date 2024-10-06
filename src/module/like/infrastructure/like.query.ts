3bf44fc26082e26dd3f6b8501b93149ab0c56bec
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-10-06 13:41:00 -0500

    feat: find one like by user and character for detail character

--- src/module/like/infrastructure/like.query.ts
+++ src/module/like/infrastructure/like.query.ts
@@ -1,13 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
-import { useLikeByUser } from "../domain/like.funtions";
 
-export function userFindLikesByUserQuery() {
+import { QUERY_KEYS } from "@/constants";
+
+import { useLikeByUser } from "../domain/like.funtions";
+import { findLikeByCharacterAndUser } from "../domain/like.respository";
+
+/**
+ * Find all likes of the current user.
+ *
+ * @returns {Promise<Like[]>} The likes of the current user.
+ */
+export function useFindLikesByUser() {
   const queryFn = useLikeByUser();
 
   return useQuery({
-    queryKey: ["likes-by-user"],
+    queryKey: [QUERY_KEYS.LIKES_BY_USER],
     queryFn,
-    refetchOnWindowFocus: false,
-    refetchOnMount: false,
+  });
+}
+
+/**
+ * Find a like by character id and user id.
+ *
+ * @param {number} characterId - The id of the character.
+ * @returns {Promise<Like>} The like or null if not found.
+ */
+export function useFindOneLikesByUserAndCharacter(characterId?: string) {
+  return useQuery({
+    queryKey: [QUERY_KEYS.LIKES_BY_USER, characterId],
+    enabled: Boolean(characterId),
+    queryFn: () => findLikeByCharacterAndUser(Number(characterId)),
   });
 }
