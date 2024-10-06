1f12a924006265c2a1daf2e028b0b8a54fac6434
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-10-06 14:12:02 -0500

    feat: add navigation for character details page

--- src/components/character/character.tsx
+++ src/components/character/character.tsx
@@ -1,7 +1,9 @@
-import type { Character } from "@/module/character/domain/character.type";
+import { Link } from "react-router-dom";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { CharacterFooter } from "./footer";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { PATHS } from "@/constants";
+import type { Character } from "@/module/character/domain/character.type";
+import { CharacterLikeButton } from "./like-button";
 
 type Props = {
   character: Character;
@@ -14,14 +16,18 @@ export function Character({ character, likeId }: Props) {
 
   return (
     <Card key={id} className="overflow-hidden">
-      <img src={src} alt={name} className="w-full h-[300px] object-cover" />
-      <CardHeader>
-        <CardTitle>{name}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p className="text-sm text-gray-600 line-clamp-4">{description || "No description"}</p>
-      </CardContent>
-      <CharacterFooter characterId={id} likeId={likeId} />
+      <Link to={`${PATHS.CHARACTER_DETAIL}/${id}`}>
+        <img src={src} alt={name} className="w-full h-[300px] object-cover" />
+        <CardHeader>
+          <CardTitle>{name}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-sm text-gray-600 line-clamp-4">{description || "No description"}</p>
+        </CardContent>
+      </Link>
+      <CardFooter className="flex flex-col items-start gap-2">
+        <CharacterLikeButton characterId={id} likeId={likeId} />
+      </CardFooter>
     </Card>
   );
 }
