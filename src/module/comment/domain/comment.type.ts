837b1bc6a86ad98e2afca4bbf4888687e91f99a3
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-10-06 20:26:58 -0500

    feat: update comment type

--- src/module/comment/domain/comment.type.ts
+++ src/module/comment/domain/comment.type.ts
@@ -1,12 +1,16 @@
 export type Comment = {
   characterId: number;
   comment: string;
+  id: string;
+  userId: string;
+  user: {
+    id: string;
+    username: string;
+  };
   createAt: Date;
   deleteAt: Date | null;
-  id: string;
   state: boolean;
   updateAt: Date;
-  userId: string;
 };
 
 export type CreateComment = {
