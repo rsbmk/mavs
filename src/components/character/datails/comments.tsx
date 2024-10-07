84080255eee63e944a1b0ac31410c79a74b06069
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-10-06 19:17:54 -0500

    feat: show comments components

---
+++ src/components/character/datails/comments.tsx
@@ -0,0 +1,57 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+import { useFindCommentByCharacter } from "@/module/comment/infrastructure/comment.query";
+import { CharacterDetailCommentsPlaceholder } from "../placeholders";
+
+type Props = {
+  characterId?: string;
+};
+
+export function Comments({ characterId }: Props) {
+  const { data: comments, isPending, isError } = useFindCommentByCharacter(characterId);
+
+  if (isError) {
+    return (
+      <ScrollArea className="w-full h-64 mb-4">
+        <h3 className="text-sm font-semibold italic">We could not recover the comments, perhaps because you are not logged in</h3>
+      </ScrollArea>
+    );
+  }
+
+  if (isPending) {
+    return (
+      <>
+        <h3 className="text-xl font-semibold mb-4">Comments</h3>
+        <CharacterDetailCommentsPlaceholder />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h3 className="text-xl font-semibold mb-4">Comments</h3>
+      <ScrollArea className="w-full h-64 mb-4">
+        {comments.length > 0 ? (
+          comments.map((comment) => (
+            <div key={comment.id} className="flex items-start space-x-4 mb-4">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                <AvatarFallback>Gest</AvatarFallback>
+              </Avatar>
+              <div className="flex-1">
+                <div className="flex items-center justify-between">
+                  <h4 className="font-semibold">{comment.userId}</h4>
+                  <span className="text-sm text-gray-500">{comment.createAt.toDateString()}</span>
+                </div>
+                <p className="mt-1">{comment.comment}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <h4 className="text-sm font-semibold italic">No comments yet</h4>
+        )}
+      </ScrollArea>
+    </>
+  );
+}
--- src/pages/character-detail.tsx
+++ src/pages/character-detail.tsx
@@ -1,46 +1,35 @@
-import type { Comment } from "@/module/comment/domain/comment.type";
-
 import { Send } from "lucide-react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 
-import { CharacterPlaceholder } from "@/components/character/characterList.placeholder";
 import { Comics } from "@/components/character/datails/comics";
+import { Comments } from "@/components/character/datails/comments";
 import { HeaderCharacter } from "@/components/character/datails/header";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { CharacterPlaceholder } from "@/components/character/placeholders";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { Textarea } from "@/components/ui/textarea";
 import { URL_PARAMS } from "@/constants";
 import { useQueryCharacterBy } from "@/module/character/infrastructure/character.query";
-
-const mockComments: Comment[] = [
-  { id: 1, user: "Steve Rogers", avatar: "/placeholder.svg?height=40&width=40", content: "Tony, you've outdone yourself this time!", timestamp: "2 hours ago" },
-  { id: 2, user: "Natasha Romanoff", avatar: "/placeholder.svg?height=40&width=40", content: "Nice suit, Stark. Does it come in black?", timestamp: "1 hour ago" },
-  { id: 3, user: "Peter Parker", avatar: "/placeholder.svg?height=40&width=40", content: "Mr. Stark, this is amazing! Can I try it on?", timestamp: "30 minutes ago" },
-];
 
 export default function CharacterDetail() {
   const { characterId } = useParams<URL_PARAMS>();
   const { data: character, isError, isPending } = useQueryCharacterBy(characterId);
 
-  const [comments, setComments] = useState<Comment[]>(mockComments);
   const [newComment, setNewComment] = useState("");
 
   const handleCommentSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (newComment.trim()) {
-      const newCommentObj: Comment = {
-        id: comments.length + 1,
-        user: "Current User",
-        avatar: "/placeholder.svg?height=40&width=40",
-        content: newComment,
-        timestamp: "Just now",
-      };
-      setComments([...comments, newCommentObj]);
-      setNewComment("");
-    }
+    // e.preventDefault();
+    // if (newComment.trim()) {
+    //   const newCommentObj: Comment = {
+    //     id: "",
+    // avatar: "/placeholder.svg?height=40&width=40",
+    // content: newComment,
+    // timestamp: "Just now",
+    //   };
+    //   setComments([...comments, newCommentObj]);
+    //   setNewComment("");
+    // }
   };
 
   if (isError) {
@@ -72,24 +61,7 @@ export default function CharacterDetail() {
           <Comics comics={comics} />
         </CardContent>
         <CardFooter className="flex flex-col items-start">
-          <h3 className="text-xl font-semibold mb-4">Comments</h3>
-          <ScrollArea className="w-full h-64 mb-4">
-            {comments.map((comment) => (
-              <div key={comment.id} className="flex items-start space-x-4 mb-4">
-                <Avatar>
-                  <AvatarImage src={comment.avatar} alt={comment.user} />
-                  <AvatarFallback>{comment.user[0]}</AvatarFallback>
-                </Avatar>
-                <div className="flex-1">
-                  <div className="flex items-center justify-between">
-                    <h4 className="font-semibold">{comment.user}</h4>
-                    <span className="text-sm text-gray-500">{comment.timestamp}</span>
-                  </div>
-                  <p className="mt-1">{comment.content}</p>
-                </div>
-              </div>
-            ))}
-          </ScrollArea>
+          <Comments characterId={characterId} />
           <form onSubmit={handleCommentSubmit} className="w-full">
             <div className="flex items-center space-x-2">
               <Textarea placeholder="Add a comment..." value={newComment} onChange={(e) => setNewComment(e.target.value)} className="flex-1" />
