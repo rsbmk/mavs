9878341e14edd6bc0c349c53052bdb958619433e
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-10-07 22:48:34 -0500

    feat: add header to layout

--- src/components/ui/layout.tsx
+++ src/components/ui/layout.tsx
@@ -1,7 +1,14 @@
+import { Header } from "../sections/header";
+
 type Props = {
   children: React.ReactNode;
 };
 
 export function Layout({ children }: Props) {
-  return <main className="px-8 py-5">{children}</main>;
+  return (
+    <main className="px-8 py-5">
+      <Header />
+      {children}
+    </main>
+  );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,10 @@
-import { Header } from "@/components/sections/header";
+import { CharacterList } from "@/components/sections/characterList";
 import { Filters } from "@/components/sections/filters";
 import { Layout } from "@/components/ui/layout";
-import { CharacterList } from "@/components/sections/characterList";
 
 export default function Home() {
   return (
     <Layout>
-      <Header />
       <Filters />
       <CharacterList />
     </Layout>
